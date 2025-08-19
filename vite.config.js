2cab6192a7496e3fb50fb43a7e6c164a99f84ad8
thealan771 <gurrolaalan771@gmail.com>
2025-08-19 15:11:34 -0600

    Configurar deploy de GitHub Pages y actualizar base de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Web_Carpinteria/'  // reemplaza con el nombre exacto de tu repo
+});
